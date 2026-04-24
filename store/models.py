2537c555cc6bed074139d99253262dabd79fa1f4
Veda-Swaroop <swaroopv91@outlook.com>
2026-04-24 18:42:23 +0530

    building products page

--- store/models.py
+++ store/models.py
@@ -1,4 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=30, unique=True, default="Undefined")
+    price = models.IntegerField(default=0)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- store/views.py
+++ store/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
 
 
-dummy_data = [
-    {
-        "item_name": "Product 1",
-        "category": "abcd",
-        "price": "20,000 Rupees",
-        "description": "samsung company smartphone",
-    },
-    {
-        "item_name": "Product 2",
-        "category": "abdhy",
-        "price": "80,000 Rupees",
-        "description": "Apple company smartphone",
-    },
-    {
-        "item_name": "Product 3",
-        "category": "andh",
-        "price": "50,000 Rupees",
-        "description": "Google company smartphone",
-    },
-]
-
-
 def home(request):
-    context = {"data": dummy_data}
+    context = {"data": Product.objects.all()}
     return render(request, "store/home.html", context)
 
 
@@ -39,5 +19,5 @@ def contact(request):
 
 
 def products(request):
-    context = {"data": dummy_data}
+    context = {"data": Product.objects.all()}
     return render(request, "store/products.html", context)
